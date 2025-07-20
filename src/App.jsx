eeda63cfb05ee15836254fb47dcdaad324fe99df
adhamElhabashy <adhammohamed8321@gmail.com>
2025-07-20 20:54:02 +0300

    feature: add About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./Section/Hero/Hero";
 import Navbar from "./Section/Navbar/Navbar";
+import About from "./Section/About/About";
 function App() {
 	return (
 		<main className="max-w-7xl mx-auto">
 			<Navbar />
 			<Hero />
+			<About />
 		</main>
 	);
 }
