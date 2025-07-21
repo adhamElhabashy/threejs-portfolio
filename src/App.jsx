f1c4670f10ac706e9789e35b2c68c6e8e69f5ea0
adhamElhabashy <adhammohamed8321@gmail.com>
2025-07-21 16:11:06 +0300

    fix: make contact button in hero refer to next section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from "./Section/Hero/Hero";
 import Navbar from "./Section/Navbar/Navbar";
 import About from "./Section/About/About";
+import Projects from "./Section/Projects/Projects";
 function App() {
 	return (
 		<main className="max-w-7xl mx-auto">
 			<Navbar />
 			<Hero />
 			<About />
+			<Projects />
 		</main>
 	);
 }
