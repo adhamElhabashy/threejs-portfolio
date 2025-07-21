1634f487d561f9f810278d0c028e0bfde9b8e658
adhamElhabashy <adhammohamed8321@gmail.com>
2025-07-21 16:28:15 +0300

    feature: add clients section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Hero from "./Section/Hero/Hero";
 import Navbar from "./Section/Navbar/Navbar";
 import About from "./Section/About/About";
 import Projects from "./Section/Projects/Projects";
+import Clients from "./Section/Clients/Clients";
+
 function App() {
 	return (
 		<main className="max-w-7xl mx-auto">
@@ -9,6 +11,7 @@ function App() {
 			<Hero />
 			<About />
 			<Projects />
+			<Clients />
 		</main>
 	);
 }
