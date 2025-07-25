5c61c717b2a6cb3548de38586c26f6aca912bba2
adhamElhabashy <adhammohamed8321@gmail.com>
2025-07-25 21:19:03 +0300

    feature: fix homepage layout

--- src/Section/About/About.jsx
+++ src/Section/About/About.jsx
@@ -12,16 +12,16 @@ function About() {
 	}
 	return (
 		<section className="c-space my-20" id="about">
-			<div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+			<div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
 				<div className="col-span-1 xl:row-span-3">
-					<div className="grid-container">
+					<div className="grid-container h-full flex flex-col">
 						<img
 							src="/assets/grid1.png"
 							alt="grid-1"
-							className="w-full sm:h-[276px] h-fit object-contain"
+							className="w-full h-[300px] object-cover"
 						/>
-						<div>
-							<p className="grid-headtext">Hi, I am Adham</p>
+						<div className="flex flex-col flex-grow justify-between space-y-2">
+							<p className="grid-headtext  mb-1">Hi, I am Adham</p>
 							<p className="grid-subtext">
 								Lorem ipsum dolor sit amet consectetur adipisicing elit.
 								Cupiditate accusamus obcaecati sapiente aperiam iure eum,
@@ -32,22 +32,22 @@ function About() {
 					</div>
 				</div>
 				<div className="col-span-1 xl:row-span-3">
-					<div className="grid-container">
+					<div className="grid-container h-full flex flex-col">
 						<img
 							src="/assets/grid2.png"
 							alt="grid-2"
-							className="w-full sm:w-[276px] h-fit object-contain"
+							className="w-full h-[300px] object-cover"
 						/>
-						<div>
-							<p className="grid-headtext">Tech Stack</p>
+						<div className="flex flex-col flex-grow  space-y-2">
+							<p className="grid-headtext  ">Tech Stack</p>
 							<p className="grid-subtext">
 								I specialize in Javascript with focus on React ecosystem
 							</p>
 						</div>
 					</div>
 				</div>
-				<div className="col-span-1 xl:row-span-4 ">
-					<div className="grid-container">
+				<div className="col-span-1">
+					<div className="grid-container h-full flex flex-col">
 						<div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
 							<Globe
 								height={326}
@@ -60,7 +60,7 @@ function About() {
 								bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
 							/>
 						</div>
-						<div>
+						<div className="flex flex-col flex-grow justify-between space-y-2">
 							<p className="grid-headtext">
 								I work remotely across most timezones
 							</p>
@@ -72,13 +72,13 @@ function About() {
 					</div>
 				</div>
 				<div className="xl:col-span-2 xl-row-span-3 ">
-					<div className="grid-container">
+					<div className="grid-container h-full flex flex-col">
 						<img
 							src="/assets/grid3.png"
 							alt="grid-3"
-							className="w-full sm:h-[266px] h-fit object.contain"
+							className="w-full h-[300px] object-cover"
 						/>
-						<div>
+						<div className="flex flex-col flex-grow justify-between space-y-2">
 							<p className="grid-headtext">My passion for coding</p>
 							<p className="grid-subtext">
 								I Love Building websites, and communicate with people.
@@ -87,11 +87,11 @@ function About() {
 					</div>
 				</div>
 				<div className="xl:col-span-1 xl:row-span-2">
-					<div className="grid-container">
+					<div className="grid-container h-full flex flex-col">
 						<img
 							src="/assets/grid4.png"
 							alt="grid-4"
-							className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+							className="w-full h-[276px] md:h-[276px] sm:h-[250px] object-contain"
 						/>
 						<div className="space-y-2">
 							<p className="grid-subtext text-center">Contact Me</p>
